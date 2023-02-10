34efb9e907125df9138ba90bdd95f5284f013ac2
Thomas Vachuska <tom.vachuska@intel.com>
2023-02-10 14:23:45 -0800

    Created initial scale-sim umbrella chart for fabric-sim and discovery-agent instances

---
+++ scale-sim/tests/onos-umbrella.go
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2020 Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package tests
+
+import (
+	"github.com/onosproject/helmit/pkg/helm"
+	"github.com/onosproject/helmit/pkg/input"
+	"github.com/onosproject/helmit/pkg/test"
+	"github.com/onosproject/onos-test/pkg/onostest"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+// OnosUmbrellaSuite is the scale-sim chart test suite
+type OnosUmbrellaSuite struct {
+	test.Suite
+	c *input.Context
+}
+
+// SetupTestSuite sets up the onos umbrella test suite
+func (s *OnosUmbrellaSuite) SetupTestSuite(c *input.Context) error {
+	s.c = c
+	return nil
+}
+
+// TestInstall tests installing the scale-sim chart
+func (s *OnosUmbrellaSuite) TestInstall(t *testing.T) {
+	registry := s.c.GetArg("registry").String("")
+	onos := helm.Chart("scale-sim", onostest.OnosChartRepo).
+		Release("scale-sim").
+		WithTimeout(15*time.Minute).
+		Set("import.onos-cli.enabled", false).
+		Set("global.image.registry", registry)
+
+	assert.NoError(t, onos.Install(true))
+}
